60bf79b8d54c83783d325a1e3608ed41282fabe7
DarionBlake <thasupabooking@gmail.com>
2023-07-10 13:17:48 -0700

    L

--- main.h
+++ main.h
@@ -3,10 +3,8 @@
 #include <stdio.h>
 #include <stdarg.h>
 
-// Include any necessary header files
 #include <stdio.h>
 
-// Function prototypes
 int _printf(const char *format, ...);
 
 #endif /* MAIN_H */
